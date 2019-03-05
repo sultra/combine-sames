3b9f9113f9ead11a844b4e7257bf271a942a3e57
zly <zhonglingye@jkinfo.com.cn>
2019-03-05 10:32:46 +0800

    test: 测试新结构和多数据逻辑

--- src/test/index.ts
+++ src/test/index.ts
@@ -23,20 +23,37 @@ const data2 = {
   msg: 'tianxia'
 }
 
+const data3 = {
+  deviceId: '123456789',
+  monitorTypeId: 701003,
+  msg: 'dashan'
+}
+
+const data4 = {
+  deviceId: '123456789',
+  monitorTypeId: 701001,
+  msg: 'niubi2'
+}
+
 //  main.inputDataIntoGroup(data);
-main.inputDataIntoGroupAutoReset(data);
-
+let res=main.inputDataIntoGroupAutoReset(data);
+console.log('1 ',res);
 delay(2000).then((rev)=>{
+  // const theKey = main.getTheKey(data);
+  // const cg: ICombine = main.combineGroup.get(theKey) || new CombineGroup([9, 8, 7, 6]);
+  // console.log('insert 1',cg.getCurrentProgress());
   // main.inputDataIntoGroup(data2);
-  main.inputDataIntoGroupAutoReset(data2);
-
-  const theKey = main.getTheKey(data);
-  const cg: ICombine = main.combineGroup.get(theKey) || new CombineGroup([9, 8, 7, 6])
-  console.log(cg.getCurrentProgress());
-  console.log(cg.getTotalProgress());
-
+  res=main.inputDataIntoGroupAutoReset(data2);
+  console.log('2 ', res);
+  // console.log('insert 2',cg.getCurrentProgress());
+  // console.log(cg.getTotalProgress());
+  res = main.inputDataIntoGroupAutoReset(data3);
+  console.log('3 ', res);
+  res = main.inputDataIntoGroupAutoReset(data4);
+  console.log('4 ', res);
   setTimeout(() => {
     console.log('end');
-    console.log(main.getGroupDataWhenProgress(theKey));
+    // console.log(cg.getCurrentProgress());
+    // console.log(cg.getTotalProgress());
   }, 8000);
 });
