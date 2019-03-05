62ec345f68c16f977ee095b188e71a253c92d8bd
zly <zhonglingye@jkinfo.com.cn>
2019-03-05 11:30:59 +0800

    test: 测试反复和无效数据时情况

--- src/test/index.ts
+++ src/test/index.ts
@@ -35,22 +35,39 @@ const data4 = {
   msg: 'niubi2'
 }
 
+const data5 = {
+  deviceId: '123456789',
+  monitorTypeId: 709912,
+  msg: 'niubi2'
+}
+
 //  main.inputDataIntoGroup(data);
-let res=main.inputDataIntoGroupAutoReset(data);
+let res=main.inputDataIntoGroup(data);
 console.log('1 ',res);
-delay(2000).then((rev)=>{
+delay(1000).then((rev)=>{
   // const theKey = main.getTheKey(data);
   // const cg: ICombine = main.combineGroup.get(theKey) || new CombineGroup([9, 8, 7, 6]);
   // console.log('insert 1',cg.getCurrentProgress());
   // main.inputDataIntoGroup(data2);
-  res=main.inputDataIntoGroupAutoReset(data2);
+  res=main.inputDataIntoGroup(data2);
   console.log('2 ', res);
   // console.log('insert 2',cg.getCurrentProgress());
   // console.log(cg.getTotalProgress());
-  res = main.inputDataIntoGroupAutoReset(data3);
+  res = main.inputDataIntoGroup(data3);
   console.log('3 ', res);
-  res = main.inputDataIntoGroupAutoReset(data4);
-  console.log('4 ', res);
+  res = main.inputDataIntoGroup(data4);
+  console.log('2-1 ', res);
+
+  res = main.inputDataIntoGroup(data2);
+  console.log('2-2 ', res);
+  // console.log('insert 2',cg.getCurrentProgress());
+  // console.log(cg.getTotalProgress());
+  
+  res = main.inputDataIntoGroup(data5);
+  console.log('5 ', res);
+
+  res = main.inputDataIntoGroup(data3);
+  console.log('2-3 ', res);
   setTimeout(() => {
     console.log('end');
     // console.log(cg.getCurrentProgress());
